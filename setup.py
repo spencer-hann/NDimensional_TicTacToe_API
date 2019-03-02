4585294969298cc0a36eac86e7567493fcd440e4
spencer <spencer.hann@gmail.com>
2019-03-02 01:46:12 -0800

    edited setup.py to compile tictactoe.pyx

---
+++ setup.py
@@ -0,0 +1,52 @@
+#========================================================================#
+#      REPLACE module_name WITH THE NAME OF YOUR CYTHON MODULE           #
+#========================================================================#
+module_name = "tictactoe" #  should not include .pyx'         #
+#========================================================================#
+
+
+import sys
+cy_name = module_name + ".pyx"
+
+
+# displays bar across console
+from os import popen
+_, console_width = popen('stty size', 'r').read().split()
+console_width = int(console_width)
+print('=' * console_width)
+
+
+print("Setup script for", module_name)
+
+
+## Default compile options ##
+ # This block checks for Cython setup arguments
+ # you can provide your own if you want to test out different
+ # options and this block will be mainly ignored
+if len(sys.argv) == 1: # no command line args
+    print(cy_name + " compiling with default args")
+    print("\tadding \"build_ext\" and \"--inplace\" to sys.argv\n")
+    sys.argv.extend( ["build_ext", "--inplace"] )
+
+
+# This block compiles/sets up the hw10 module
+# from the  hw10.pyx cython file
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+print("   Cython may give a depricated NumPy API warning. This warning is safe to ignore.\n")
+setup(
+    ext_modules = cythonize(
+        Extension(
+            module_name,
+            [cy_name],
+            define_macros=[("NPY_NO_DEPRECATED_API",None)]
+        )
+    )
+)
+
+
+print(module_name + " setup complete!")
+
+# displays bar across console
+print('=' * console_width)
