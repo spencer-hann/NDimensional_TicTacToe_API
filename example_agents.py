be949574edf0ca57a94574143806b11467e6dd28
spencer <spencer.hann@gmail.com>
2019-03-08 03:20:32 -0800

    NaiveBestFirstAgent w/smart block created

--- example_agents.py
+++ example_agents.py
@@ -6,7 +6,46 @@ class RandomAgent:
         pass # very simple agent, no internal state
 
     def next_move(self, game):
-        while True:
+        for i in range(100): # eventually just give up
             coord = tuple(np.random.randint(0,game.size,size=game.dim))
-            if game.board[coord] == game.empty_square:
-                return coord
+            if game.is_empty_here(coord):
+                break
+        return coord
+
+class NaiveBestFirstAgent:
+    def __init__(self, marker, smart_block=False):
+        self.marker = marker
+        self.smart_block = smart_block
+
+    def next_move(self, game):
+        best_endpair = None
+        best_score = 0
+
+        # seach for most in-a-row currently
+        for endpair,score in game._lines.items():
+            if score > best_score:
+                best_endpair = endpair
+                best_score = score
+            # or block opponent one move from winning
+            if -score == game.size-1 and self.smart_block:
+                best_endpair = endpair
+                break
+
+        # if none found, take middle of board
+        if best_endpair == None:
+            middle = tuple(np.full(game.dim, game.size//2))
+            if game.is_empty_here(middle):
+                return middle
+            return tuple(np.random.randint(0,game.size,size=game.dim))
+
+        # search for open spot between endpoints pair
+        slope = game.determine_slope(*endpair)
+        search_square = np.asarray(best_endpair[0])
+
+        while (abs(search_square) < game.size).all():
+            if game.is_empty_here(tuple(search_square)):
+                return tuple(search_square)
+            search_square += slope
+
+        return tuple(np.random.randint(0,game.size,size=game.dim))
+
--- tictactoe.py
+++ tictactoe.py
@@ -1,16 +1,13 @@
 from tictactoe import Game
-from example_agents import RandomAgent
+from example_agents import RandomAgent, NaiveBestFirstAgent
 
 def two_player_game(g):
-    print("building game...",end=' ')
     g.display_board()
-    print("done!")
 
     turn = 0
     while 1:
         turn += 1
-        print("Turn",turn)
-        print('-'*20)
+        print('-'*10,"Turn",turn,'-'*10)
         winner = g.take_turn()
         g.display_board()
         if winner:
@@ -18,15 +15,12 @@ def two_player_game(g):
             break
 
 def human_vs_ai_game(game,ai):
-    print("building game...",end=' ')
     game.display_board()
-    print("done!")
 
     turn = 0
     while 1:
         turn += 1
-        print("Turn",turn)
-        print('-'*20)
+        print('-'*10,"Turn",turn,'-'*10)
         winner = game.take_turn() # for user
         game.display_board()
         if winner:
@@ -40,15 +34,12 @@ def human_vs_ai_game(game,ai):
             break
 
 def ai_vs_ai_game(game,ai1,ai2):
-    print("building game...",end=' ')
     game.display_board()
-    print("done!")
 
     turn = 0
     while 1:
         turn += 1
-        print("\nTurn",turn)
-        print('-'*20)
+        print('-'*10,"Turn",turn,'-'*10)
         winner = game.take_turn(ai1.next_move(game))
         game.display_board()
         if winner:
@@ -56,6 +47,7 @@ def ai_vs_ai_game(game,ai1,ai2):
             break
 
         winner = game.take_turn(ai2.next_move(game))
+        print() # new line
         game.display_board()
         if winner:
             print(winner,"is the winner!!!")
@@ -63,6 +55,9 @@ def ai_vs_ai_game(game,ai1,ai2):
 
 
 if __name__ == "__main__":
-    g = Game(size=3,dim=3)
-    human_vs_ai_game(g,RandomAgent())
-    #ai_vs_ai_game(g,RandomAgent(),RandomAgent())
+    print("building game...",end=' ')
+    g = Game(size=4,dim=3)
+    print("done!")
+    #human_vs_ai_game(g,RandomAgent())
+    #ai_vs_ai_game(g,RandomAgent(), NaiveBestFirstAgent('X'))
+    ai_vs_ai_game(g,RandomAgent(), NaiveBestFirstAgent('X',smart_block=True))
