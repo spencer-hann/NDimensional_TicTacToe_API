2702c39f00e55ad3fe3b3554751324eb26386c32
spencer <spencer.hann@gmail.com>
2019-03-20 02:54:34 -0700

    added rule-based agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-from .example_agents import NaiveBestFirstAgent, RandomAgent
+from .example_agents import NaiveBestFirstAgent, RandomAgent, RuleBasedAgent
 from .reinforcement_agent import ReinforcementAgent
 
 sys.path.append('..')  # Why do you suck so much, python importing
--- agents/example_agents.py
+++ agents/example_agents.py
@@ -13,28 +13,60 @@ class RandomAgent:
         if game.is_full():
             raise Exception("Random Agent trying to play on full board")
         return tuple(game.random_empty_square())
-        #while 1:
-        #for i in range(100): # eventually just give up
-        #    coord = tuple(np.random.randint(0,game.size,size=game.dim))
-        #    if game.is_empty_here(coord):
-        #        break
-        #return coord
 
-# NaiveBestFirstAgent is incomplete.
-# currently only able to play as 'X'
 class NaiveBestFirstAgent:
-    def __init__(self, marker, smart_block=False):
+    def __init__(self, marker):
         self.name = "Naive Best First"
         self.marker = marker
-        self.smart_block = smart_block
+
+    def next_move(self, game):
+        best_endpair = None
+        best_score = 0
+        point_value = game.determine_point_value(self.marker)
+
+        # seach for most in-a-row currently
+        for endpair,score in game._lines.items():
+            score *= point_value
+            # only adjust if not about to lose else where (smart_block)
+            if score > best_score:
+                best_endpair = endpair
+                best_score = score
+
+        # if none found, take middle of board
+        if best_endpair == None:
+            middle = tuple(np.full(game.dim, game.size//2))
+            if game.is_empty_here(middle):
+                return middle
+            # if middle is taken choose at random
+            return tuple(game.random_empty_square())
+
+        # search for open spot between endpoints pair
+        slope = game.determine_slope(*best_endpair)
+        search_square = np.asarray(best_endpair[0])
+
+        while (abs(search_square) < game.size).all():
+        #while (search_square < game.size).all() and (search_square >= 0).all():
+            if game.is_empty_here(tuple(search_square)):
+                return tuple(search_square)
+            search_square += slope
+
+        return tuple(game.random_empty_square())
+
+class RuleBasedAgent:
+    def __init__(self, marker, skill_level=3):
+        self.name = "Rule-Based"
+        self.marker = marker
+        self.skill_level = skill_level
 
     def next_move(self, game):
         best_endpair = None
         best_score = 0
         score_lock = False
+        point_value = game.determine_point_value(self.marker)
 
         # seach for most in-a-row currently
         for endpair,score in game._lines.items():
+            score *= point_value
             # only adjust if not about to lose else where (smart_block)
             if score > best_score and not score_lock:
                 best_endpair = endpair
@@ -43,7 +75,11 @@ class NaiveBestFirstAgent:
                 best_endpair = endpair
                 break
             # or block opponent one move from winning
-            if -score == game.size-1 and self.smart_block:
+            if self.skill_level >= 2 and -score == game.size-1:
+                best_endpair = endpair
+                score_lock = True
+            # stop opponent from creating trap
+            if self.skill_level >= 3 and -score == game.size-2 and not score_lock:
                 best_endpair = endpair
                 score_lock = True
 
